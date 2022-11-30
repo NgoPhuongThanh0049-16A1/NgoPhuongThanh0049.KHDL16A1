cfc1a88ae510806ddabd2de9929362b859554290
PThanh0049 <117415122+ngophuongthanh0309@users.noreply.github.com>
2022-11-30 16:25:06 +0700

    Add files via upload

---
+++ 8.17.py
@@ -0,0 +1,7 @@
+# Tính BCLN(a,b)
+a = int(input("Nhập số a = "))
+b = int(input("Nhập số b = "))
+def bscnn(a, b):
+    return int(a * b) 
+    uscln(a, b)
+print("Bội số chung nhỏ nhất của", a, "và", b, "là:", bscnn(a, b))
